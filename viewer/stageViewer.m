7ce93cb4cd189e698d77afa56e631b508ea69704
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2019-05-29 17:03:30 +0100

    progress made but needs more to do!

--- viewer/stageViewer.m
+++ viewer/stageViewer.m
@@ -1,26 +1,16 @@
 function out = stageViewer(o, stagingData)
 img = '.\img\background_boundaries.tif'
-Roi = stagingData.Roi;
-SpotGeneName = o.GeneNames(o.SpotCodeNo);
-uGenes = unique(SpotGeneName);
 
-% which ones pass quality threshold (combi first)
-QualOK = o.quality_threshold;
-
-% now show only those in Roi
-if ~isempty(Roi)
-    InRoi = all(o.SpotGlobalYX>=Roi([3 1]) & o.SpotGlobalYX<=Roi([4 2]),2);
-else
-    InRoi = true;
-end
-
-PlotSpots = find(InRoi & QualOK);
-[~, GeneNo] = ismember(SpotGeneName(PlotSpots), uGenes);
+cd = cleanData(o, stagingData);
+uGenes = cd.uGenes;
+PlotSpots = cd.PlotSpots;
+GeneNo = cd.GeneNo;
 
 stagingData.allSpots = collectSpots(o, uGenes, PlotSpots, GeneNo);
 
 collectData(stagingData, o);
 
+Roi = stagingData.Roi;
 xRange = 1+Roi(2)-Roi(1);
 yRange = 1+Roi(4)-Roi(3);
 scaleFactor = 32768/max(xRange, yRange);
@@ -41,9 +31,36 @@ cmdStr = ['.\vips\bin\vips.exe gravity ' bigImg ' ' tilesFolder '[layout=google,
 system(cmdStr);
 fprintf('%s: Done! \n', datestr(now));
 
-system ('javaw -jar ./jar/SimpleWebServer.jar')
-dos('start http://localhost:80/');
+system ('start http://localhost:80')
+system ('java -jar ./jar/SimpleWebServer.jar')
 
         
 
 end
+
+
+function out = cleanData(o, stagingData)
+
+Roi = stagingData.Roi;
+SpotGeneName = o.GeneNames(o.SpotCodeNo);
+uGenes = unique(SpotGeneName);
+
+% which ones pass quality threshold (combi first)
+QualOK = o.quality_threshold;
+
+% now show only those in Roi
+if ~isempty(stagingData.Roi)
+    InRoi = all(o.SpotGlobalYX>=stagingData.Roi([3 1]) & o.SpotGlobalYX<=stagingData.Roi([4 2]),2);
+else
+    InRoi = true;
+end
+
+PlotSpots = find(InRoi & QualOK);
+[~, GeneNo] = ismember(SpotGeneName(PlotSpots), uGenes);
+
+out.uGenes = uGenes;
+out.PlotSpots = PlotSpots;
+out.GeneNo = GeneNo;
+
+end
+
