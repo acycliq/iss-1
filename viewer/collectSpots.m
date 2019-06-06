9fafb952a4540b04ab8d8817c26294f2907b7b06
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2019-06-06 13:33:16 +0100

    clean up

--- viewer/collectSpots.m
+++ viewer/collectSpots.m
@@ -1,5 +1,5 @@
 function out = collectSpots(o, uGenes, PlotSpots, GeneNo)
-fprintf('%s: Function collectSpots was called. \n', datestr(now));
+% fprintf('%s: Function collectSpots was called. \n', datestr(now));
 
 
 allSpots = [];
@@ -10,11 +10,5 @@ for i=1:length(uGenes)
     mat2 = repmat(uGenes(i),nn,1);
     allSpots = [allSpots; [mat2, num2cell(mat1)]];
 end
-% save('allSpots.mat', 'allSpots')
-% fprintf('%s: allSpots.mat saved \n', datestr(now))
-% % T = cell2table(myarr);
-% % T.Properties.VariableNames = {'Gene','Expt','y','x'};
-% % save('mySpots.mat', 'T')
-% % % writetable(T, 'Dapi_overlays.csv');
 
 out = allSpots;
